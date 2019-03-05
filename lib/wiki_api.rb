3fa4592fe024322288dd9edf78ca2b69f91c018e
becc-mu <becc.mulugeta@gmail.com>
2019-03-05 12:06:38 +0000

    cleaned up rubocop offences

--- lib/wiki_api.rb
+++ lib/wiki_api.rb
@@ -1,25 +1,24 @@
 require 'askwiki'
-
+# retrieves data from the API
 class WikiAPI
-
-  def ask query 
-    query_encoded = URI::encode(query)
+  def ask(query)
+    query_encoded = URI.encode(query)
     request_url = "http://#{@language}.wikipedia.org/w/api.php?action=parse&page=#{query_encoded}&format=json&prop=text&section=0&redirects"
     text = ''
     OpenURI.allow_redirect do
       open(request_url) do |file|
-        text = JSON.parse(file.read())['parse']['text'].first[1]
-          .gsub(/<\/?[^>]+>/, '') # strip tags
-          .gsub(/[[:space:]]+/, ' ') # strip whitespace
-          .gsub(/&#[0-9]+;/,'') # strip encoded
-          .gsub(/\[[0-9]+\]/,'') # strip referencing
+        text = JSON.parse(file.read)['parse']['text'].first[1]
+                   .gsub(%r{</?[^>]+>}, '') # strip tags
+                   .gsub(/[[:space:]]+/, ' ') # strip whitespace
+                   .gsub(/&#[0-9]+;/, '') # strip encoded
+                   .gsub(/\[[0-9]+\]/, '') # strip referencing
       end
     end
     text
   end
 end
 
-def self.ask( query, lang='en' )
+def self.ask(query, lang = 'en')
   ask_obj = Askwiki.new lang
   ask_obj.ask query
 end
--- spec/wiki_api_spec.rb
+++ spec/wiki_api_spec.rb
@@ -4,8 +4,7 @@ describe WikiAPI do
   subject(:wikAPI) { described_class.new }
 
   it 'should return a page given url' do
-    page = subject.ask('john lennon') 
-    page2 = "https://en.wikipedia.org/wiki/John_Lennon"
-    expect(page.title).to eq ('John_Lennon')
+    page = subject.ask('john lennon')
+    expect(page.title).to eq 'John_Lennon'
   end
 end
