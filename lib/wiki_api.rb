06382a95c72961bc1b0e1aaed67180d7044a5f18
becc-mu <becc.mulugeta@gmail.com>
2019-03-05 10:54:27 +0000

    fetching api

--- lib/wiki_api.rb
+++ lib/wiki_api.rb
@@ -1,3 +1,25 @@
+require 'askwiki'
+
 class WikiAPI
 
+  def ask query 
+    query_encoded = URI::encode(query)
+    request_url = "http://#{@language}.wikipedia.org/w/api.php?action=parse&page=#{query_encoded}&format=json&prop=text&section=0&redirects"
+    text = ''
+    OpenURI.allow_redirect do
+      open(request_url) do |file|
+        text = JSON.parse(file.read())['parse']['text'].first[1]
+          .gsub(/<\/?[^>]+>/, '') # strip tags
+          .gsub(/[[:space:]]+/, ' ') # strip whitespace
+          .gsub(/&#[0-9]+;/,'') # strip encoded
+          .gsub(/\[[0-9]+\]/,'') # strip referencing
+      end
+    end
+    text
+  end
+end
+
+def self.ask( query, lang='en' )
+  ask_obj = Askwiki.new lang
+  ask_obj.ask query
 end
--- spec/wiki_api_spec.rb
+++ spec/wiki_api_spec.rb
@@ -1,8 +1,11 @@
 require 'wiki_api'
+
 describe WikiAPI do
+  subject(:wikAPI) { described_class.new }
+
   it 'should return a page given url' do
-    page = Wikipedia.find('John Lennon')
+    page = subject.ask('john lennon') 
     page2 = "https://en.wikipedia.org/wiki/John_Lennon"
-    expect(page.title).to eq page2
+    expect(page.title).to eq ('John_Lennon')
   end
 end
